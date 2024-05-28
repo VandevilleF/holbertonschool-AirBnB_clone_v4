2932a802d4671a79cd1ebd6652ef3067045c6cff
VandevilleF <flo.vandeville@gmail.com>
2024-05-28 16:31:37 +0200

    change of logic at the beginning of the code

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,25 +1,23 @@
 $(document).ready(function() {
-  const selectedAmenities = {};
+    let selectedAmenities = {};
+    $('input[type="checkbox"]').change(function() {
+        let amenityId = $(this).data('id');
+        let amenityName = $(this).data('name');
+        if ($(this).prop('checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+        let selectedAmenitiesText = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text(selectedAmenitiesText);
+    });
 
-  $('input[type="checkbox"]').change(function() {
-      const amenityId = $(this).attr('data-id');
-      const amenityName = $(this).attr('data-name');
-
-      if (this.checked) {
-          selectedAmenities[amenityId] = amenityName;
-      } else {
-          delete selectedAmenities[amenityId];
-      }
-
-      const amenitiesList = Object.values(selectedAmenities).join(', ');
-      $('div.amenities h4').text(amenitiesList);
-  });
-
-  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-      if (data.status === "OK") {
-          $('#api_status').addClass('available');
-      } else {
-          $('#api_status').removeClass('available');
-      }
-  });
+    $.get("http://127.0.0.1:5001/api/v1/status/", function(data, textStatus)
+    {
+        if (data.status === "OK") {
+            $('#api_status').addClass("available");
+        } else {
+            $('#api_status').removeClass("available");
+        }
+    });
 });
